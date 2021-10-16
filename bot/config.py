3b17324d525b0790a00cf9df48442dd6d604862e
titu3e <75441242+titu3e@users.noreply.github.com>
2021-10-16 14:13:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int("")
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = int("8626831")
+    API_HASH = "db23330a6edf4a517ee186b35cedec71"
+    BOT_TOKEN = "2090713720:AAHHSOguRRGVphl-fsLGJ39FMsmrot59FmA"
